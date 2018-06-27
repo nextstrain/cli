11f1c2089cb30a199a1435e56bfe7e0983ef4e4d
Thomas Sibley <tsibley@fredhutch.org>
2018-06-27 11:05:45 -0700

    Refactor code for running commands under docker into a module
    This starts to cleanly separate the high-level concerns of the "build"
    command from the implementation details of the environment in which
    snakemake is run.  It also provides a foundation for sharing the docker
    runner with other commands, like the "view" command I'm about to add.
    
    The idea is that eventually there will be other runner modules that the
    "build" command can use seamlessly, such as runner.local or
    runner.singularity.

--- nextstrain/cli/command/build.py
+++ nextstrain/cli/command/build.py
@@ -14,89 +14,28 @@ build interface from Docker itself so that we can more seamlessly use other
 container systems in the future as desired or necessary.
 """
 
-import os
-import subprocess
-import argparse
-from ..util import warn
-
-
-COMPONENTS = ["sacra", "fauna", "augur"]
+from ..runner import docker
 
 
 def register_parser(subparser):
     parser = subparser.add_parser("build", help = "Run pathogen build")
     parser.description = __doc__
 
-    # Development options
-    development = parser.add_argument_group(
-        "development options",
-        "These should generally be unnecessary unless you're developing build images.")
-
-    development.add_argument(
-        "--image",
-        help    = "Container image in which to run the pathogen build",
-        metavar = "<name>",
-        default = "quay.io/nextstrain/base")
-
-    development.add_argument(
-        "--exec",
-        help    = "Program to exec inside the build container",
-        metavar = "<prog>",
-        default = "snakemake")
-
-    for component in COMPONENTS:
-        development.add_argument(
-            "--" + component,
-            help    = "Replace the image's copy of %s with a local copy" % component,
-            metavar = "<dir>")
-
-    development.add_argument(
-        "--docker-arg",
-        help    = "Additional arguments to pass to `docker run`",
-        metavar = "...",
-        dest    = "docker_args",
-        action  = "append")
-
     # Positional parameters
     parser.add_argument(
         "directory",
         help    = "Path to pathogen build directory",
-        metavar = "<directory>")
+        metavar = "<directory>",
+        action  = docker.store_volume("build"))
 
-    parser.add_argument(
-        "extra_args",
-        help    = "Additional arguments to pass to the executed build program",
-        metavar = "...",
-        nargs   = argparse.REMAINDER)
+    # Runner options
+    docker.register_arguments(
+        parser,
+        exec    = ["snakemake", ...],
+        volumes = ["sacra", "fauna", "augur"])
 
     return parser
 
 
 def run(opts):
-    volumes = [
-        ('build', opts.directory),
-        *[(c, getattr(opts, c)) for c in COMPONENTS]
-    ]
-
-    argv = [
-        "docker", "run",
-        "--rm",
-        "--tty",            # Colors, etc.
-        "--interactive",    # Pass through control signals (^C, etc.)
-        "--user=%d:%d" % (os.getuid(), os.getgid()),
-      *["--volume=%s:/nextstrain/%s" % (os.path.abspath(src), dst)
-            for dst, src in volumes
-             if src is not None],
-        *opts.docker_args,
-        opts.image,
-        opts.exec,
-        *opts.extra_args
-    ]
-
-    try:
-        subprocess.run(argv, check = True)
-    except subprocess.CalledProcessError as e:
-        warn("Error running %s, exited %d" % (e.cmd, e.returncode))
-        return e.returncode
-    else:
-        return 0
+    return docker.run(opts)
---
+++ nextstrain/cli/runner/docker.py
@@ -0,0 +1,129 @@
+"""
+Run commands inside a container image using Docker.
+"""
+
+import os
+import argparse
+import subprocess
+from collections import namedtuple
+from ..util import warn
+
+
+def store_volume(volume_name):
+    """
+    Generates and returns an argparse.Action subclass for storing named volume
+    tuples.
+
+    Multiple argparse arguments can use this to cooperatively accept source
+    path definitions for named volumes.
+
+    Each named volume is stored as a namedtuple (name, src).  The tuple is
+    stored on the options object under the volume's name (modified to replace
+    slashes with underscores), as well as added to a shared list of volumes,
+    accessible via the "volumes" attribute on the options object.
+    """
+    volume = namedtuple("volume", ("name", "src"))
+
+    class store(argparse.Action):
+        def __call__(self, parser, namespace, values, option_strings = None):
+            # Add the new volume to the list of volumes
+            volumes    = getattr(namespace, "volumes", [])
+            new_volume = volume(volume_name, values)
+            setattr(namespace, "volumes", [*volumes, new_volume])
+
+            # Allow the new volume to be found by name on the opts object
+            setattr(namespace, volume_name.replace('/', '_'), new_volume)
+
+    return store
+
+
+def register_arguments(parser, exec=None, volumes=[]):
+    # Unpack exec parameter into the command and everything else
+    (exec_cmd, *exec_args) = exec
+
+    # Development options
+    development = parser.add_argument_group(
+        "development options",
+        "These should generally be unnecessary unless you're developing build images.")
+
+    development.add_argument(
+        "--image",
+        help    = "Container image in which to run the pathogen build",
+        metavar = "<name>",
+        default = "quay.io/nextstrain/base")
+
+    development.add_argument(
+        "--exec",
+        help    = "Program to exec inside the build container",
+        metavar = "<prog>",
+        default = exec_cmd)
+
+    for name in volumes:
+        development.add_argument(
+            "--" + name,
+            help    = "Replace the image's copy of %s with a local copy" % name,
+            metavar = "<dir>",
+            action  = store_volume(name))
+
+    development.add_argument(
+        "--docker-arg",
+        help    = "Additional arguments to pass to `docker run`",
+        metavar = "...",
+        dest    = "docker_args",
+        action  = "append")
+
+    # Optional exec arguments
+    parser.set_defaults(exec_args = exec_args)
+    parser.set_defaults(extra_exec_args = [])
+
+    if ... in exec_args:
+        parser.add_argument(
+            "extra_exec_args",
+            help    = "Additional arguments to pass to the executed build program",
+            metavar = "...",
+            nargs   = argparse.REMAINDER)
+
+
+def run(opts):
+    if opts.docker_args is None:
+        opts.docker_args = []
+
+    argv = [
+        "docker", "run",
+        "--rm",             # Remove the ephemeral container after exiting
+        "--tty",            # Colors, etc.
+        "--interactive",    # Pass through control signals (^C, etc.)
+
+        # Run the process in the container with the same UID/GID so that file
+        # ownership is correct in the bind mount directories.
+        "--user=%d:%d" % (os.getuid(), os.getgid()),
+
+        # Map directories to bind mount into the container.
+      *["--volume=%s:/nextstrain/%s" % (os.path.abspath(v.src), v.name)
+            for v in opts.volumes
+             if v.src is not None],
+
+        *opts.docker_args,
+        opts.image,
+        opts.exec,
+        *replace_ellipsis(opts.exec_args, opts.extra_exec_args)
+    ]
+
+    try:
+        subprocess.run(argv, check = True)
+    except subprocess.CalledProcessError as e:
+        warn("Error running %s, exited %d" % (e.cmd, e.returncode))
+        return e.returncode
+    else:
+        return 0
+
+
+def replace_ellipsis(items, elided_items):
+    """
+    Replaces any Ellipsis items (...) in a list, if any, with the items of a
+    second list.
+    """
+    return [
+        y for x in items
+          for y in (elided_items if x is ... else [x])
+    ]
