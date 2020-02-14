d4e7486ffec522fcd9eb28578ff53fa0a9801f78
Thomas Sibley <tsibley@fredhutch.org>
2020-02-14 15:40:11 -0800

    deploy: Note in usage that this is an alias for "remote upload"

--- nextstrain/cli/command/deploy.py
+++ nextstrain/cli/command/deploy.py
@@ -4,9 +4,20 @@
 # Registering our own parser lets us preserve the original short description
 # and avoids introducing "upload" as a top-level command.
 
+from textwrap import dedent
 from .remote.upload import register_arguments, run, __doc__
 
 def register_parser(subparser):
     parser = subparser.add_parser("deploy", help = "Deploy pathogen build")
     register_arguments(parser)
     return parser
+
+def insert_paragraph(text, index, insert):
+    paras = text.split("\n\n")
+    paras.insert(index, dedent(insert))
+    return "\n\n".join(paras)
+
+__doc__ = insert_paragraph(
+    __doc__, 1, """
+    The `nextstrain deploy` command is an alias for `nextstrain remote upload`.
+    """)
