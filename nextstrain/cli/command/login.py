d8c6444239fbd0a7e1a8b13b026d5887084a57c8
Kairsten Fay <kairsten.fay@gmail.com>
2020-06-24 09:37:34 -0700

    login: Enhance functionality when already logged in
    If the Nextstrain.org Groups user already has credentials stored at
    ~/.nextstrain/secrets, don't immediately prompt them for a password when
    they login subsequent times. Verify their saved tokens, refreshing and
    re-saving them if necessary. Prompt for a password only when token
    verification fails.

--- nextstrain/cli/command/login.py
+++ nextstrain/cli/command/login.py
@@ -21,7 +21,56 @@ def register_parser(subparser):
 def run(opts):
     print("Logging into nextstrain.org...")
 
-    login()
+    tokens = {
+        k : config.get('login', k, fallback=None, path=config.SECRETS)
+            for k in [ 'username' ,'access_token', 'id_token', 'refresh_token' ]
+    }
+
+    if not tokens['username']:
+        login()
+        return 0
+
+    try:
+        authenticate_without_password(tokens)
+
+    except Exception as e:
+        error_message = "Failed to log in with saved credentials: %s" % e
+
+        print(error_message)
+        print()
+        login()
+        return 0
+
+    print("Logged into nextstrain.org as %s" % tokens['username'])
+    print("Log out with `nextstrain logout`")
+
+
+def authenticate_without_password(tokens: Dict[str, str]):
+    """
+    Authenticates a Nextstrain.org Groups user with the provided *tokens*.
+    """
+    cognito = Cognito(COGNITO_USER_POOL_ID, COGNITO_CLIENT_ID,
+        username=tokens['username'],
+        id_token=tokens['id_token'],
+        refresh_token=tokens['refresh_token'],
+        access_token=tokens['access_token'])
+
+    # The Cognito.authenticate() method, which performs token verification,
+    # requires a password. We do not want to store passwords locally or prompt
+    # a Nextstrain.org Groups user for a password each time they login
+    # (otherwise, what's the point of storing credentials locally?).
+    #
+    # So, as a workaround for authenticating AWS SRP tokens, utilize the
+    # undocumented Cognito.verify_token() method which will throw an error if
+    # the given token is invalid.
+    cognito.verify_token(tokens['id_token'], 'id_token', 'id')
+
+    # The Cognito.check_token() method verifies the access token, so there's no
+    # need to use the .verify_token() workaround here.
+    access_token_expired = cognito.check_token()
+    if access_token_expired:
+        print("Replacing expired tokens...")
+        save_credentials(cognito, config.SECRETS)
 
 
 def login():
