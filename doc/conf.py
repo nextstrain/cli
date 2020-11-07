aad2b1ef46029a7c5f78b2958edeeb93f5d00be6
eharkins <eli.harkins@gmail.com>
2020-11-07 14:14:37 -0800

    include aws doc fix

--- doc/conf.py
+++ doc/conf.py
@@ -44,7 +44,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
-    'aws-batch.md',
     'development.md'
 ]
 
