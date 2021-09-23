94f92d3f67fd43a8b43976a45b82e518fa3bdd08
Thomas Sibley <tsibley@fredhutch.org>
2021-09-23 12:53:49 -0700

    errors: Support interpolation into UserErrors _after_ multi-line dedent
    This helps with some kinds of messages where the interpolation expands
    to multiple-lines that would otherwise interfere with dedent.

--- nextstrain/cli/errors.py
+++ nextstrain/cli/errors.py
@@ -9,9 +9,9 @@ class NextstrainCliError(Exception):
     pass
 
 class UserError(NextstrainCliError):
-    def __init__(self, message):
+    def __init__(self, message, *args, **kwargs):
         # Remove leading newlines, trailing whitespace, and then indentation
         # to better support nicely-formatted """multi-line strings""".
-        formatted_message = dedent(message.lstrip("\n").rstrip())
+        formatted_message = dedent(message.lstrip("\n").rstrip()).format(*args, **kwargs)
 
         super().__init__("Error: " + formatted_message)
