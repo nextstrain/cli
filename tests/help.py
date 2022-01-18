c347e7c30cd0fd72381e42b40408c9f71a73d530
Thomas Sibley <tsibley@fredhutch.org>
2022-01-18 12:13:03 -0800

    dev: Test --help for all commands
    Ensures that all commands can load and don't throw runtime errors
    outputting --help.

---
+++ tests/help.py
@@ -0,0 +1,29 @@
+import pytest
+import argparse
+from itertools import chain
+from nextstrain.cli import make_parser
+from subprocess import run
+
+
+def walk_commands(command, parser):
+    yield command
+
+    subparsers = chain.from_iterable(
+        action.choices.items()
+            for action in parser._actions
+             if isinstance(action, argparse._SubParsersAction))
+
+    for subname, subparser in subparsers:
+        yield from walk_commands([*command, subname], subparser)
+
+
+commands = list(walk_commands(("nextstrain",), make_parser()))
+
+
+@pytest.mark.parametrize("command", commands, ids = lambda command: " ".join(command))
+def pytest_help(command):
+    # Check the exit status ourselves for nicer test output on failure
+    argv = (*command, "--help")
+
+    result = run(argv)
+    assert result.returncode == 0, f"{argv} exited with error"
