75164d4823068b0b49464b84e2e017c36d2bd9e9
Thomas Sibley <tsibley@fredhutch.org>
2022-07-18 15:39:07 -0700

    docs: Enable the copybutton extension we enable with our Sphinx theme
    Our theme enables and configures the copybutton extension, but this only
    works if our theme is also enabled as an extension (not just a theme).

--- doc/conf.py
+++ doc/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx_markdown_tables',
     'sphinxarg.ext',
+    'nextstrain.sphinx.theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
