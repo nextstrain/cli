a94a856d2f28d713318de66a51086f9fb066d547
Thomas Sibley <tsibley@fredhutch.org>
2022-09-23 16:54:48 -0700

    paths: Use a from_env() helper function to reduce verbosity of each path definition
    Matters more the more paths we define, and a few more are on their way.

--- nextstrain/cli/paths.py
+++ nextstrain/cli/paths.py
@@ -3,6 +3,18 @@ Filesystem paths.
 """
 import os
 from pathlib import Path
+from typing import Union
+
+
+def from_env(name: str, default: Union[str, Path]) -> Path:
+    """
+    Wrap a :py:cls:`Path` around the value of the environment variable *name*,
+    if any, otherwise *default*.
+
+    Environment variables which are set but empty will be treated as unset
+    (i.e. *default* will be used).
+    """
+    return Path(os.environ.get(name) or default)
 
 
 # Not finding a homedir is unlikely, but possible.  Fallback to the current
@@ -13,13 +25,10 @@ except:
     HOME = Path(".")
 
 # Path to our config file
-CONFIG = Path(os.environ.get("NEXTSTRAIN_CONFIG") or
-              HOME / ".nextstrain/config")
+CONFIG = from_env("NEXTSTRAIN_CONFIG", HOME / ".nextstrain/config")
 
 # Path to our secrets file
-SECRETS = Path(os.environ.get("NEXTSTRAIN_SECRETS") or
-               HOME / ".nextstrain/secrets")
+SECRETS = from_env("NEXTSTRAIN_SECRETS", HOME / ".nextstrain/secrets")
 
 # Path to our global lock file
-LOCK = Path(os.environ.get("NEXTSTRAIN_LOCK") or
-            HOME / ".nextstrain/lock")
+LOCK = from_env("NEXTSTRAIN_LOCK", HOME / ".nextstrain/lock")
