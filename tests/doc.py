4aaf67b6a78cdd7ad7a24248e518ed988c01d7c5
Thomas Sibley <tsibley@fredhutch.org>
2023-07-14 15:50:06 -0700

    dev: Show a diff of generated doc when our test checking it's up-to-date fails
    Very helpful to see what's different.  I built the diffing into
    generate-command-doc as a new --diff option since it will probably be
    handy in local dev too in combination with --check.

--- tests/doc.py
+++ tests/doc.py
@@ -5,5 +5,5 @@ topdir = Path(__file__).resolve().parent.parent
 
 def pytest_generated_command_doc():
     # Check the exit status ourselves for nicer test output on failure
-    result = run([topdir / "devel/generate-command-doc", "--check"])
+    result = run([topdir / "devel/generate-command-doc", "--check", "--diff"])
     assert result.returncode == 0, f"{result.args!r} exited with errors"
