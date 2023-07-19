4ad5e8e1e7ddeebe913de5b3397ffe750e6028f6
Thomas Sibley <tsibley@fredhutch.org>
2023-07-19 12:04:57 -0700

    dev: Skip command doc generation test on Windows
    Our development tooling requires a POSIX platform.

--- tests/doc.py
+++ tests/doc.py
@@ -1,8 +1,11 @@
+import os
+import pytest
 from pathlib import Path
 from subprocess import run
 
 topdir = Path(__file__).resolve().parent.parent
 
+@pytest.mark.skipif(os.name != "posix", reason = "devel/generate-command-doc requires a POSIX platform")
 def pytest_generated_command_doc():
     # Check the exit status ourselves for nicer test output on failure
     result = run([topdir / "devel/generate-command-doc", "--check", "--diff"])
