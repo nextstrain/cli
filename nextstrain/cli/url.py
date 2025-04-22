c5a243d04dccfe84053c2d2199cf5a1367a848a7
Thomas Sibley <tsibley@fredhutch.org>
2025-04-22 14:02:02 -0700

    url: Support joining with a base URL during construction
    This can be handy for canonicalizing potentially relative URLs, e.g.
    Location or Link HTTP response header values.

--- nextstrain/cli/url.py
+++ nextstrain/cli/url.py
@@ -5,7 +5,7 @@ Extended forms of :mod:`urllib.parse` with an API that fits a little better.
 """
 import os
 from typing import List, Mapping, NewType, Optional, Union
-from urllib.parse import urlsplit, SplitResult, parse_qs as parse_query, urlencode as construct_query
+from urllib.parse import urlsplit, SplitResult, parse_qs as parse_query, urlencode as construct_query, urljoin
 
 
 Origin = NewType("Origin", str)
@@ -38,17 +38,26 @@ class URL(SplitResult):
 
     >>> u.origin
     'https://example.org'
+
+    An optional *base* URL string or :cls:`URL` is accepted and combined with
+    *url* using :func:`urllib.parse.urljoin` before parsing.
+
+    >>> URL("def?x=y#z", "https://example.com:123/abc/")
+    URL(scheme='https', netloc='example.com:123', path='/abc/def', query='x=y', fragment='z')
+
+    >>> URL("https://example.com/abc/def?x=y#z", "https://example.com:123/xyz?n=42#abc")
+    URL(scheme='https', netloc='example.com', path='/abc/def', query='x=y', fragment='z')
     """
     __slots__ = ()
 
-    def __new__(cls, url: str) -> 'URL':
-        return super().__new__(cls, *urlsplit(url))
+    def __new__(cls, url: str, base: Union[str, 'URL'] = None) -> 'URL':
+        return super().__new__(cls, *urlsplit(urljoin(str(base), url) if base else url))
 
     # This is for the type checkers, which otherwise consider URL.__init__ to
     # have a signature based on SplitResult.__new__ instead of our own __new__.
     # It's not clear to me *why* they do that, but this hint helps sort it out.
     #   -trs, 16 Nov 2023
-    def __init__(self, url: str) -> None: ...
+    def __init__(self, url: str, base: Union[str, 'URL'] = None) -> None: ...
 
     def __str__(self) -> str:
         return self.geturl()
