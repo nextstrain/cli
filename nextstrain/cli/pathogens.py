2973069fa9f49eaaba37babbaa9c4a26d0433427
Jover Lee <joverlee521@gmail.com>
2025-07-18 15:43:44 -0700

    PathogenVersion: read pathogen registration during init and setup
    Saves the contents of the pathogen registration as an attribute during the 
    instantiation of an existing pathogen and during setup of a new pathogen.
    
    Doing this in anticipation of using the pathogen registration to list a 
    pathogen's available workflows.

--- nextstrain/cli/pathogens.py
+++ nextstrain/cli/pathogens.py
@@ -130,6 +130,7 @@ class PathogenVersion:
 
     setup_receipt: Optional[dict] = None
     url: Optional[URL] = None
+    registration: Optional[dict] = None
 
 
     def __init__(self, name_version_url: str, new_setup: bool = False):
@@ -242,6 +243,9 @@ class PathogenVersion:
 
         self.registration_path  = self.path / "nextstrain-pathogen.yaml"
         self.setup_receipt_path = self.path.with_suffix(self.path.suffix + ".json")
+
+        if self.registration_path.exists():
+            self.registration = read_pathogen_registration(self.registration_path)
 
         if not new_setup:
             if not self.path.is_dir():
@@ -448,6 +452,8 @@ class PathogenVersion:
             json.dump(self.setup_receipt, f, indent = "  ")
             print(file = f)
 
+        self.registration = read_pathogen_registration(self.registration_path)
+
         return True
 
 
@@ -455,12 +461,11 @@ class PathogenVersion:
         def test_compatibility() -> SetupTestResult:
             msg = "nextstrain-pathogen.yaml declares `nextstrain run` compatibility"
 
-            registration = read_pathogen_registration(self.registration_path)
-            if registration is None:
+            if self.registration is None:
                 return msg + "\n(couldn't read registration)", False
 
             try:
-                compatibility = registration["compatibility"]["nextstrain run"]
+                compatibility = self.registration["compatibility"]["nextstrain run"]
             except (KeyError, IndexError, TypeError):
                 if DEBUGGING:
                     traceback.print_exc()
