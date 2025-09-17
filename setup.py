a806d1ed202b298d571920c1ee31536357c6e7b6
Thomas Sibley <tsibley@fredhutch.org>
2025-09-16 22:08:57 -0700

    dev: Explicitly support (i.e. test) Python 3.13
    3.13 is almost a year old.

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 
     # Install a "nextstrain" program which calls nextstrain.cli.__main__.main()
